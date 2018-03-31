5e5bc232e4e4fb7e51e965835fcec631dc3e1f4c
Stuart Raynor <madstu@gmail.com>
2018-03-31 22:20:01 +0100

    Add files via upload

--- ZOCinstall.sh
+++ ZOCinstall.sh
@@ -33,7 +33,7 @@ sudo chmod -R 777 $HOME/.zeroonecore
 RPCU=$(pwgen -1 4 -n)
 PASS=$(pwgen -1 14 -n)
 EXIP=$(curl ipinfo.io/ip)
-printf "rpcuser=rpc$RPCU\nrpcpassword=$PASS\nrpcport=9998\nrpcthreads=8\nrpcallowip=127.0.0.1\nbind=$EXIP:10000\nmaxconnections=128\ngen=0\nexternalip=$EXIP\ndaemon=1\n\naddnode=128.199.62.128\naddnode=159.65.24.219\naddnode=188.166.91.38\naddnode=174.138.10.244\naddnode=128.199.62.128\naddnode=103.69.195.185:58848\naddnode=13.56.174.183:10000\naddnode=13.59.197.120:10000\naddnode=144.202.17.171:10000\naddnode=174.138.10.244:10000\naddnode=185.224.249.58:45325\naddnode=213.183.51.7:10000\naddnode=45.32.6.178:34558\naddnode=62.77.153.115:10000\naddnode=79.165.207.31:57377\naddnode=90.145.172.88:10000\naddnode=90.156.157.28:10000\n\n" > /$HOME/.zeroonecore/zeroone.conf
+printf "rpcuser=rpc$RPCU\nrpcpassword=$PASS\nrpcport=9998\nrpcthreads=8\nrpcallowip=127.0.0.1\nbind=$EXIP:10000\nmaxconnections=128\ngen=0\nexternalip=$EXIP\ndaemon=1\n\naddnode=77.68.95.52:10000\naddnode=54.38.79.148:10000\naddnode=77.68.76.112:10000\naddnode=174.138.10.244:10000\naddnode=103.69.195.185:10000\naddnode=13.56.174.183:10000\naddnode=13.59.197.120:10000\naddnode=144.202.17.171:10000\naddnode=174.138.10.244:10000\naddnode=185.224.249.58:10000\naddnode=45.32.6.178:10000\naddnode=79.165.207.31:10000\naddnode=90.145.172.88:10000\naddnode=90.156.157.28:10000\n\n" > /$HOME/.zeroonecore/zeroone.conf
 zerooned -daemon
 sleep 10
 MKEY=$(zeroone-cli masternode genkey)
