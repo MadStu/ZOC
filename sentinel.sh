9ee207d9a9ecd724a3e65f79502477d2c349116b
Stuart Raynor <madstu@gmail.com>
2018-09-19 08:40:20 +0100

    Create sentinel.sh

---
+++ sentinel.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+cd ~
+sudo apt-get -y update
+sudo apt-get -y install virtualenv python-virtualenv
+git clone https://github.com/zocteam/sentinel.git zoc_sentinel
+cd zoc_sentinel
+virtualenv ./venv
+./venv/bin/pip install -r requirements.txt
+crontab -l > mycron
+echo "* * * * * cd $(pwd) && ./venv/bin/python bin/sentinel.py >/dev/null 2>&1" >> mycron
+crontab mycron
+rm mycron
